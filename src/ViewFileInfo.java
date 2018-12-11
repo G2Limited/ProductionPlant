320e65493ef415fc83bc3d41c1fb4aabd457f204
Garrett <ggraber7402@eagle.fgcu.edu>
2018-12-11 15:51:25 -0500

    Update ViewFileInfo.java

--- src/ViewFileInfo.java
+++ src/ViewFileInfo.java
@@ -2,11 +2,15 @@ import java.io.FileReader;
 
 public class ViewFileInfo {
 
+  /**
+   * Create a driver class named ViewFileInfo that will read the information from
+   * the TestResults.txt file and display the contents to the console.
+   */
   public static void main(String[] args) {
     try {
-    FileReader fr = new FileReader("C:\\LineTests\\TestResults.txt");
+      FileReader fileReader = new FileReader("C:\\LineTests\\TestResults.txt");
       int i;
-      while ((i = fr.read()) != -1) {
+      while ((i = fileReader.read()) != -1) {
         System.out.print((char) i);
       }
     } catch (Exception ex) {
